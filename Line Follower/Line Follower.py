889881266446b19e111ecd83597a815713a1c4d9
Scott Firth <41399244+scottyman09@users.noreply.github.com>
2021-11-16 21:23:11 +1100

    Create Line Follower.py

--- Line Follower/Line Follower.py
+++ Line Follower/Line Follower.py
@@ -1,3 +1,34 @@
+#!/usr/bin/env python3
+
+# Import the necessary libraries
+import time
+import math
+from ev3dev2.motor import *
+from ev3dev2.sound import Sound
+from ev3dev2.sensor import *
+from ev3dev2.sensor.lego import *
+from ev3dev2.sensor.virtual import *
+
+# Create the sensors and motors objects
+motorA = LargeMotor(OUTPUT_A)
+motorB = LargeMotor(OUTPUT_B)
+left_motor = motorA
+right_motor = motorB
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
+steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
+
+spkr = Sound()
+radio = Radio()
+
+color_sensor_in1 = ColorSensor(INPUT_1)
+ultrasonic_sensor_in2 = UltrasonicSensor(INPUT_2)
+gyro_sensor_in3 = GyroSensor(INPUT_3)
+gps_sensor_in4 = GPSSensor(INPUT_4)
+pen_in5 = Pen(INPUT_5)
+
+motorC = LargeMotor(OUTPUT_C) # Magnet
+
+# Here is where your code starts
 while True:
     offset = color_sensor_in1.reflected_light_intensity - 50
     steering_drive.on(offset * 2, 20)
